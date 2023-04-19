5c3f52d02e5255582fd6bcf8a3c5ebb377e4366a
Lothar Rubusch <l.rubusch@gmail.com>
2023-04-19 05:07:33 +0100

    docker: fix installation
    - interactive setup.sh via expect
    - separation into tools such as SEGGER, SDK, python - onboard
    - zephyr sources by requirements.txt - mounted
    
    In order to obtain the zephyr/requirements.txt, currently
    in the zephyrproject (mountpoint) a "west init" is done. In
    runtime this folder's content will be overmounted. The
    python packages are supposed to be installed already, though.
    
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

---
+++ docker/build_context/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -e
+
+export MY_ZEPHYR_SDK_VERSION="0.16.0"
+
+export MY_CONFIGS="/home/$USER/configs"
+export MY_ZEPHYR_SDK="zephyr-sdk-${MY_ZEPHYR_SDK_VERSION}_linux-x86_64.tar.xz"
+export MY_ZEPHYR_DIR="/home/$USER/zephyrproject"
+export MY_ZEPHYR_SDK_DIR="/home/$USER/zephyr-sdk-${MY_ZEPHYR_SDK_VERSION}"
+
+## permissions
+00_defenv.sh "${MY_ZEPHYR_DIR}"
+
+## initial clone zephyr sources
+if [ ! -e "${MY_ZEPHYR_DIR}/.west" ]; then
+	cd "/home/$USER"
+	pip install west
+	west init ${MY_ZEPHYR_DIR}
+fi
+cd ${MY_ZEPHYR_DIR}
+
+## update zephyr base
+west update
+west zephyr-export
+pip install -r ${MY_ZEPHYR_DIR}/zephyr/scripts/requirements.txt
+
+echo "READY."
